2ed3f3a5681237afb55c97e403fbaddc2515f535
PRAKHAR <PRAKHAR@DESKTOP-NE16FA8>
2021-08-12 22:34:08 +0530

    Refatoring and can take two inputs, 1 inputs as well as 0 inputs.

--- Incubyte_String_Calculator/src/Main/String_Calculator.java
+++ Incubyte_String_Calculator/src/Main/String_Calculator.java
@@ -3,12 +3,18 @@ package Main;
 public class String_Calculator {
 	public int Add(String numbers)
 	{
-		int result = 1;
+		String[] arr = numbers.split(",");
+		int result = 1;char ch = ',';
 		if(numbers.isEmpty())
-			result = 0;
-		else 
 		{
-			String[] arr = numbers.split(",");
+			result = 0;
+		}
+		else if(arr.length == 1)
+		{
+			result = Integer.parseInt(numbers);
+		}
+		else
+		{
 			result = Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]);
 		}
 		return result;
